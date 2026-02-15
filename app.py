44492f8b555b9ec73f75126600f1d48d4a4a24b4
techlearnermaitri <moremaitri677699@gmail.com>
2026-02-15 18:45:18 +0530

    react to api

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 
 import os
-import pymupdf as pdf
-import pandas as pd
-import base64
-from datetime  import datetime
+from fastapi import FastAPI , UploadFile, File , HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_community.vectorstores import Chroma
+import google.generativeai as genai
+import json
 
+app=FastAPI()
 
-#pdf extractor
+#react frontend to api
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
