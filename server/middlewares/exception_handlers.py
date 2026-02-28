0d0bc302b73a6a34d3fc71097b55bb93b4ef2946
techlearnermaitri <moremaitri677699@gmail.com>
2026-02-28 22:33:55 +0530

    Prompt Template

--- server/middlewares/exception_handlers.py
+++ server/middlewares/exception_handlers.py
@@ -8,4 +8,4 @@ async def catch_exceptions_middleware(request:Request, call_next):
         return await call_next(request)
     except Exception as exc:
         logger.exception("unhandled exception: %s", exc)
-        return JSONResponse(status_code=500, content={"error" str(exc)})
+        return JSONResponse(status_code=500, content={"error": str(exc)})
---
+++ server/modules/llm.py
@@ -0,0 +1,57 @@
+#this will handle llm response and prompt
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
+
+def get_llm_chain(retriever):
+    llm=ChatGroq(
+        api_key=GROQ_API_KEY,
+        model_name="llama3-70b-8192"
+    )
+    prompt = PromptTemplate(
+        input_variables=["context", "question"],
+        template="""
+        You are **InsightsPopRAG**, an AI-powered research assistant trained to answer questions based only on uploaded documents.
+
+        Your job is to provide clear, accurate, and structured answers based **only on the provided context**.
+
+        ---
+
+        🧠 **User Question**:
+        {question}
+
+        ---
+
+        📚 **Context**:
+        {context}
+
+        ---
+
+        📝 **Answer Guidelines**:
+        - Respond in a professional and structured format.
+        - Base your answer strictly on the context.
+        - If the answer is not found in the context, say:
+        "I couldn't find relevant information in the provided documents."
+        - Do NOT invent information.
+        - If possible, reference the source from the context.
+        - Keep answers concise but informative.
+
+        ---
+
+        ✅ **Answer**:
+        """
+        )
+    return RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        prompt=prompt,
+        chain_type_kwargs={"prompt": prompt}
+        return_source_documents=True     #for citatios will be goods
+    )
